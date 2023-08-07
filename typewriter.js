9a404c4db58cfb9ed655f444f8ddb388141d05d5
Faiem-qy <f.quanyoung@gmail.com>
2023-08-06 22:40:30 -0400

    updated code to log string after 1 second

--- typewriter.js
+++ typewriter.js
@@ -3,5 +3,8 @@ const sentence = "hello there from lighthouse labs";
 
 for (const char of sentence) {
   // console.log(char);
-  process.stdout.write(char)
+  // process.stdout.write(char)
+  setTimeout (() => {
+    process.stdout.write(char)
+  },1000)
 }
